0e37dfbd37967bf2c2581e74ea10d3c24032556c
David Yan <davidplusplus@gmail.com>
2014-07-08 00:52:11 -0700

    Added sample code for communicating with back-end via WebSocket

--- tenderloin/client/static/js/controllers.js
+++ tenderloin/client/static/js/controllers.js
@@ -2,6 +2,28 @@
 
   this.controller('IndexCtrl', function($scope) {
     $scope.tenderloin = 'tenderloin';
+    $scope.msgs = [];
+
+    var ws = new WebSocket('ws://' + location.host + '/api/chat');
+
+    ws.onopen = function(ev) {
+      $scope.$apply(function() {
+        $scope.opened = true;
+        $scope.send = function(msg) {
+          var obj = {
+            name: "hello",
+            message: msg
+          }
+          ws.send(JSON.stringify(obj));
+        };
+      });
+    }
+
+    ws.onmessage = function(ev) {
+      $scope.$apply(function() {
+        $scope.msgs.push(JSON.parse(ev.data));
+      });
+    }
   });
 
 }).call(angular.module('controllers', []));
