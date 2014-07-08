ffdda79bb7caa899d5f2df691956f0c694e282f8
Michael Tom-Wing <mtomwing@gmail.com>
2014-07-07 22:37:25 -0700

    Added basic tornado server to serve client files
    $ python -m tenderloin.web

---
+++ tenderloin/web/__main__.py
@@ -0,0 +1,46 @@
+"""Kind of hacky server for the API and client code."""
+
+import logging
+import os
+
+from tornado.options import define, options
+import tornado.ioloop
+import tornado.web
+
+BASE_PATH = os.path.dirname(os.path.dirname(__file__))
+
+define("port", default=8000, help="run on the given port", type=int)
+
+
+class SingleFileHandler(tornado.web.StaticFileHandler):
+    """FileHandler that only reads a single static file."""
+
+    @classmethod
+    def get_absolute_path(cls, root, path):
+        return tornado.web.StaticFileHandler.get_absolute_path(root,
+                                                               "index.html")
+
+
+def get_application():
+    static_path = os.path.join(BASE_PATH, 'client', 'static')
+    static_config = {'path': static_path}
+
+    application = tornado.web.Application([
+        (r"/(.*\..*)", tornado.web.StaticFileHandler, static_config),
+        (r"/(.*)", SingleFileHandler, static_config),
+    ])
+    return application
+
+
+def main():
+    logging.basicConfig(format='[%(levelname)s][%(name)s]: %(message)s')
+    logging.getLogger().setLevel(logging.DEBUG)
+
+    tornado.options.parse_command_line()
+    application = get_application()
+    application.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
